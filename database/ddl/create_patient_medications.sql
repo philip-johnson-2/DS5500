2bd845e5963e7e002c7a5d1636f1265bd43d4907
Nick <tylernickr@gmail.com>
2019-10-19 14:17:07 -0400

    Update definition

--- database/ddl/create_patient_medications.sql
+++ database/ddl/create_patient_medications.sql
@@ -1,15 +1,15 @@
 /*
     Create table patient medications
 */
-DROP TABLE patient_medications 
-GO
+DROP TABLE patient_medications;
 
 CREATE TABLE patient_medications (
     patient_id INT
+    , fill_date DATE
     , ndc_code VARCHAR(25)
-    , medication_generic_name VARCHAR(250)
+    , medication_generic_name VARCHAR(500)
     , days_supply INT
-    , quantity INT 
+    , quantity NUMERIC(30,15) 
 )
 
 CREATE INDEX idx_patient_medications_patient_id ON patient_medications (patient_id)
