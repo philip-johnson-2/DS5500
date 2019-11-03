d980b37dc9e3a2d899e453a8f22045fa825f6938
philip-johnson-2 <56003568+philip-johnson-2@users.noreply.github.com>
2019-11-03 11:33:00 -0500

    added other events

--- database/ddl/create_event_wrk.sql
+++ database/ddl/create_event_wrk.sql
@@ -6,7 +6,14 @@ CREATE TABLE event_wrk
  prev_admission_6mo INT,
  prev_admission_3mo INT,
  prev_admission_1mo INT,
- prev_admission_idx INT,
+ prev_er_12mo INT,
+ prev_er_6mo INT,
+ prev_er_3mo INT,
+ prev_er_1mo INT,
+ prev_pcp_12mo INT,
+ prev_pcp_6mo INT,
+ prev_pcp_3mo INT,
+ prev_pcp_1mo INT,
  fut_admission_12mo INT,
  fut_admission_6mo INT,
  fut_admission_3mo INT,
@@ -15,17 +22,27 @@ CREATE TABLE event_wrk
 
 insert into event_wrk
 select patient_id,
-	MAX(case when event_date between '2024-06-30' and '2025-06-30' then 1 else 0 end),
-	MAX(case when event_date between '2024-12-31' and '2025-06-30' then 1 else 0 end),
-	MAX(case when event_date between '2025-03-30' and '2025-06-30' then 1 else 0 end),
-	MAX(case when event_date between '2025-05-30' and '2025-06-30' then 1 else 0 end),
-	0,
-	MAX(case when event_date between '2025-06-30' and '2026-06-30' then 1 else 0 end),
-	MAX(case when event_date between '2025-06-30' and '2025-12-31' then 1 else 0 end),
-	MAX(case when event_date between '2025-06-30' and '2025-09-30' then 1 else 0 end),
-	MAX(case when event_date between '2025-06-30' and '2025-07-30' then 1 else 0 end)
+	-- admissions
+	MAX(case when event_name = 'Admission' AND event_date between '2024-06-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2024-12-31' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2025-03-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2025-05-30' and '2025-06-30' then 1 else 0 end),
+	-- er visits
+	MAX(case when event_name = 'ER' AND event_date between '2024-06-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'ER' AND event_date between '2024-12-31' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'ER' AND event_date between '2025-03-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'ER' AND event_date between '2025-05-30' and '2025-06-30' then 1 else 0 end),
+	-- pcp visits 
+	MAX(case when event_name = 'PCP Visit' AND event_date between '2024-06-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'PCP Visit' AND event_date between '2024-12-31' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'PCP Visit' AND event_date between '2025-03-30' and '2025-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'PCP Visit' AND event_date between '2025-05-30' and '2025-06-30' then 1 else 0 end),
+	-- future admissions
+	MAX(case when event_name = 'Admission' AND event_date between '2025-06-30' and '2026-06-30' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2025-06-30' and '2025-12-31' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2025-06-30' and '2025-09-30' then 1 else 0 end),
+	MAX(case when event_name = 'Admission' AND event_date between '2025-06-30' and '2025-07-30' then 1 else 0 end)	
 from patient_medical_events
-where event_name = 'Admission'
 group by patient_id;
 
 CREATE INDEX idx_event_wrk_patient_id ON event_wrk (patient_id);
